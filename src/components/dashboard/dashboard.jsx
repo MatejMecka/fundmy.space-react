47e09bbf7c54deeb3082660146ae94b9916bf430
matejmecka <matej.plavevski+github@gmail.com>
2021-02-09 23:56:48 +0100

    even better look

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
 
     const useStyles = makeStyles((theme) => ({
         root: {
-          minWidth: 275,
+ //         minWidth: 275,
           padding: "50px",
         },
         thatDiv: {
@@ -29,7 +29,12 @@ export default function Dashboard() {
         },
         no_results: {
             textAlign: 'center'
-          },
+        },
+        errorMessage: {
+          display: 'flex',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+        },
         pos: {
           marginBottom: 12
         },
@@ -52,6 +57,9 @@ export default function Dashboard() {
           },
         divider: {
             border: `1px solid ${theme.palette.divider}`
+        },
+        card: {
+          paddingBottom: "25px"
         }
       }));
 
@@ -59,39 +67,39 @@ export default function Dashboard() {
 
     return (
       <div className={classes.thatDiv}>
-        <Grid container className={classes.root} spacing={2} justify="center" alignItems="center">
-            <Grid container item>
+        <Grid container className={classes.root} spacing={0 } justify="center" alignItems="center" direction="column">
+            <Grid container item spacing={5}>
             <Typography variant="h4" component="h4" className={classes.title}>Balances</Typography>
                 <Grid item container direction="row" justify="center" alignItems="center">
-                  <Grid item xs={12} sm={4}>
+                  <Grid item xs={12} sm={4} className={classes.card}>
                       <BalanceCard/>
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={4} className={classes.card}>
                       <BalanceCard/>
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={4} className={classes.card}>
                       <BalanceCard/>
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid container item justify="center" alignItems="center"> 
+            <Grid container item > 
             <Typography variant="h4" component="h4" className={classes.title}>Claimable Balances</Typography>
-                <Grid item container xs={12} >
+                <Grid container item className={classes.errorMessage} direction="row">
                     <Avatar className={classes.avatar}>
                         <SentimentVeryDissatisfiedIcon />
                     </Avatar>
+                    <Typography variant="h6" component="h4" className={classes.no_results}>No pending claimable balances.</Typography>
                 </Grid>
-                <Typography variant="h4" component="h4" className={classes.no_results}>No pending claimable balances.</Typography>
             </Grid>
 
-            <Grid container item justify="center" alignItems="center">
-            <Typography variant="h4" component="h4" className={classes.title}>Past operations</Typography>
-                <Grid item xs={12}>
-                    <Avatar className={classes.avatar}>
-                        <SentimentVeryDissatisfiedIcon />
-                    </Avatar>
-                </Grid>
-                <Typography variant="h4" component="h4" className={classes.no_results}>No past operations.</Typography>
+            <Grid container item >
+              <Typography variant="h4" component="h4" className={classes.title}>Past operations</Typography>
+              <Grid container item className={classes.errorMessage} direction="row">
+                  <Avatar className={classes.avatar}>
+                      <SentimentVeryDissatisfiedIcon />
+                  </Avatar>
+                  <Typography variant="body1" component="h4" className={classes.no_results}>No past operations.</Typography>
+              </Grid>
             </Grid>
 
         </Grid>
--- src/components/dashboard/elements/currency_card.jsx
+++ src/components/dashboard/elements/currency_card.jsx
@@ -7,7 +7,7 @@ import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275
+    minWidth: 275,
   },
   bullet: {
     display: "inline-block",
